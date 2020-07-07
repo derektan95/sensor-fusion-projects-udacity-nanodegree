76fc8c99161abd3e19961771fd21df750adfb858
derektan95 <derektan95@hotmail.com>
2020-07-07 16:48:21 +0800

    Implemented RANSAC for straight line under quiz.

--- lidar/SFND_Lidar_Obstacle_Detection/src/quiz/ransac/ransac2d.cpp
+++ lidar/SFND_Lidar_Obstacle_Detection/src/quiz/ransac/ransac2d.cpp
@@ -69,14 +69,39 @@ std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int ma
 	// TODO: Fill in this function
 
 	// For max iterations 
+	for (size_t i = 0; i < maxIterations; i++)
+	{
+		// Randomly sample subset and fit line (Add chosen ints into tempInliersResult)
+		std::unordered_set<int> tempInliersResult;
+		while (tempInliersResult.size() < 2) { tempInliersResult.insert((rand() % cloud->points.size())); }	// avoid choosing duplicate element	
+		
+		auto itr = tempInliersResult.begin();
+		pcl::PointXYZ randomPoint1 = cloud->points[*itr];
+		itr++;
+		pcl::PointXYZ randomPoint2 = cloud->points[*itr];
 
-	// Randomly sample subset and fit line
+		// Measure distance between every point and fitted line
+		double lineAValue = randomPoint1.y - randomPoint2.y;
+		double lineBValue = randomPoint2.x - randomPoint1.x;
+		double lineCValue = (randomPoint1.x * randomPoint2.y) - (randomPoint2.x * randomPoint1.y);
+		
+		for (size_t i = 0; i < cloud->points.size(); i++)
+		{
+			// Do not carry on this iteration if already in tempInliersResult
+			if (tempInliersResult.count(i) > 0) continue;
+			
+			pcl::PointXYZ point = cloud->points[i];
+			double distance = fabs((lineAValue * point.x) + (lineBValue * point.y) + (lineCValue)) / (sqrt((lineAValue*lineAValue) + (lineBValue*lineBValue)));
 
-	// Measure distance between every point and fitted line
-	// If distance is smaller than threshold count it as inlier
+			// If distance is smaller than threshold count it as inlier
+			// NOTE: Would not insert if same int being inserted
+			if (distance <= distanceTol) tempInliersResult.insert(i);
+		}
 
-	// Return indicies of inliers from fitted line with most inliers
-	
+		// Return indicies of inliers from fitted line with most inliers
+		if (tempInliersResult.size() > inliersResult.size()) inliersResult = tempInliersResult;
+	}
+
 	return inliersResult;
 
 }
@@ -92,7 +117,7 @@ int main ()
 	
 
 	// TODO: Change the max iteration and distance tolerance arguments for Ransac function
-	std::unordered_set<int> inliers = Ransac(cloud, 0, 0);
+	std::unordered_set<int> inliers = Ransac(cloud, 50, 0.5);
 
 	pcl::PointCloud<pcl::PointXYZ>::Ptr  cloudInliers(new pcl::PointCloud<pcl::PointXYZ>());
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloudOutliers(new pcl::PointCloud<pcl::PointXYZ>());
@@ -105,6 +130,7 @@ int main ()
 		else
 			cloudOutliers->points.push_back(point);
 	}
+
 
 
 	// Render 2D point cloud with inliers and outliers
