99ec979237f366384aa74555394972890bee20fe
derektan95 <derektan95@hotmail.com>
2020-07-13 21:39:39 +0800

    Completed simple exercise of computing distance with lidar, considering bounds of road hardcoded.

---
+++ camera/SFND_2D_Feature_Tracking/src/TTC_lidar/build/CMakeFiles/compute_ttc_lidar.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compute_ttc_lidar.dir/src/compute_ttc_lidar.cpp.o"
+  "CMakeFiles/compute_ttc_lidar.dir/src/structIO.cpp.o"
+  "compute_ttc_lidar.pdb"
+  "compute_ttc_lidar"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/compute_ttc_lidar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
