90589efa0b1ac53f44ae0ff61993b43473075064
derektan95 <derektan95@hotmail.com>
2020-07-15 01:14:27 +0800

    Added more descriptors from open cv library, including FAST descriptor.

---
+++ camera/SFND_2D_Feature_Tracking/src/detect_keypoints/src/detect_keypoints.cpp
@@ -0,0 +1,81 @@
+// Features from Accelerated Segment Test detector (FAST) vs Shi-Tomasi detector
+
+#include <iostream>
+#include <numeric>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/features2d.hpp>
+
+using namespace std;
+
+void detKeypoints1()
+{
+    // load image from file and convert to grayscale
+    cv::Mat imgGray;
+    cv::Mat img = cv::imread("../images/img1.png");
+    cv::cvtColor(img, imgGray, cv::COLOR_BGR2GRAY);
+
+    // Shi-Tomasi detector
+    int blockSize = 6;       //  size of a block for computing a derivative covariation matrix over each pixel neighborhood
+    double maxOverlap = 0.0; // max. permissible overlap between two features in %
+    double minDistance = (1.0 - maxOverlap) * blockSize;
+    int maxCorners = img.rows * img.cols / max(1.0, minDistance); // max. num. of keypoints
+    double qualityLevel = 0.01;                                   // minimal accepted quality of image corners
+    double k = 0.04;                                              // Harris parameter (see equation for details)
+    bool useHarris = false;
+
+    vector<cv::KeyPoint> kptsShiTomasi;
+    vector<cv::Point2f> corners;
+    double t = (double)cv::getTickCount();
+    cv::goodFeaturesToTrack(imgGray, corners, maxCorners, qualityLevel, minDistance, cv::Mat(), blockSize, useHarris, k);
+    t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+    cout << "Shi-Tomasi with n= " << corners.size() << " keypoints in " << 1000 * t / 1.0 << " ms" << endl;
+
+    for (auto it = corners.begin(); it != corners.end(); ++it)
+    { // add corners to result vector
+
+        cv::KeyPoint newKeyPoint;
+        newKeyPoint.pt = cv::Point2f((*it).x, (*it).y);
+        newKeyPoint.size = blockSize;
+        kptsShiTomasi.push_back(newKeyPoint);
+    }
+
+    // visualize results
+    cv::Mat visImage = img.clone();
+    cv::drawKeypoints(img, kptsShiTomasi, visImage, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+    string windowName = "Shi-Tomasi Results";
+    cv::namedWindow(windowName, 1);
+    imshow(windowName, visImage);
+    cv::waitKey(0);
+
+    // TODO: use the OpenCV library to add the FAST detector
+    // in addition to the already implemented Shi-Tomasi
+    // detector and compare both algorithms with regard to
+    // (a) number of keypoints, (b) distribution of
+    // keypoints over the image and (c) processing speed.
+
+    // Initialize detector container
+    int threshold = 30;                                                              // difference between intensity of the central pixel and pixels of a circle around this pixel
+    bool bNMS = true;                                                                // perform non-maxima suppression on keypoints
+    cv::FastFeatureDetector::DetectorType type = cv::FastFeatureDetector::TYPE_9_16; // TYPE_9_16, TYPE_7_12, TYPE_5_8
+    cv::Ptr<cv::FeatureDetector> detector = cv::FastFeatureDetector::create(threshold, bNMS, type);
+
+    // Perform detection
+    vector<cv::KeyPoint> kptsFAST;
+    t = (double)cv::getTickCount();
+    detector->detect(imgGray, kptsFAST);
+    t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+    cout << "FAST with n= " << kptsFAST.size() << " keypoints in " << 1000 * t / 1.0 << " ms" << endl;
+
+    visImage = img.clone();
+    cv::drawKeypoints(img, kptsFAST, visImage, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+    cv::namedWindow("FAST Results", 1);
+    imshow("FAST Results", visImage);
+    cv::waitKey(0);
+}
+
+int main()
+{
+    detKeypoints1();
+}
