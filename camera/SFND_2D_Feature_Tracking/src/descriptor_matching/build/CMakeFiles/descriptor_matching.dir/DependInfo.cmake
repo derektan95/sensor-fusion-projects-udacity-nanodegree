147f9e45307429feb63311cfa3104a9a3b1abb5f
derektan95 <derektan95@hotmail.com>
2020-07-15 18:40:23 +0800

    Added keypoint matchers to match keypoints from BRISK and SIFT using FLANN or Brute force.

---
+++ camera/SFND_2D_Feature_Tracking/src/descriptor_matching/build/CMakeFiles/descriptor_matching.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/derektan/Desktop/sensor_fusion_udacity/tutorials/camera/SFND_2D_Feature_Tracking/src/descriptor_matching/src/descriptor_matching.cpp" "/home/derektan/Desktop/sensor_fusion_udacity/tutorials/camera/SFND_2D_Feature_Tracking/src/descriptor_matching/build/CMakeFiles/descriptor_matching.dir/src/descriptor_matching.cpp.o"
+  "/home/derektan/Desktop/sensor_fusion_udacity/tutorials/camera/SFND_2D_Feature_Tracking/src/descriptor_matching/src/structIO.cpp" "/home/derektan/Desktop/sensor_fusion_udacity/tutorials/camera/SFND_2D_Feature_Tracking/src/descriptor_matching/build/CMakeFiles/descriptor_matching.dir/src/structIO.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
