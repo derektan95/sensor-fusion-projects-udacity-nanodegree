67ab064bb1228f48cf59508cb6baa19c13207237
derektan95 <derektan95@hotmail.com>
2020-07-18 13:36:53 +0800

    Added Lidar_To_camera exercise. Added color gradient and segmented ground plane, hardcoded.

---
+++ camera/SFND_2D_Feature_Tracking/src/lidar_to_camera/build/CMakeFiles/show_lidar_top_view.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/show_lidar_top_view.dir/src/show_lidar_top_view.cpp.o"
+  "CMakeFiles/show_lidar_top_view.dir/src/structIO.cpp.o"
+  "show_lidar_top_view.pdb"
+  "show_lidar_top_view"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/show_lidar_top_view.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
