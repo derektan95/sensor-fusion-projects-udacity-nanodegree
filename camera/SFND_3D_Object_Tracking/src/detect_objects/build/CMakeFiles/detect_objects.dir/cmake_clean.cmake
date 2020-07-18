ade427d7a533d9e36854c57289da167f22580039
derektan95 <derektan95@hotmail.com>
2020-07-18 20:09:31 +0800

    Attempting to push .weights file from YOLO using git lfs.

---
+++ camera/SFND_3D_Object_Tracking/src/detect_objects/build/CMakeFiles/detect_objects.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/detect_objects.dir/src/detect_objects_2.cpp.o"
+  "detect_objects.pdb"
+  "detect_objects"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/detect_objects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
