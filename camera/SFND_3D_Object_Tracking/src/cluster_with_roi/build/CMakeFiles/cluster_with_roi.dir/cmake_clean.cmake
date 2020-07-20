8138d3c68db6d6baea4ef663f7c983705d2839c1
derektan95 <derektan95@hotmail.com>
2020-07-20 15:24:46 +0800

    Clustered points based on bounding boxes.

---
+++ camera/SFND_3D_Object_Tracking/src/cluster_with_roi/build/CMakeFiles/cluster_with_roi.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cluster_with_roi.dir/src/cluster_with_roi.cpp.o"
+  "CMakeFiles/cluster_with_roi.dir/src/structIO.cpp.o"
+  "cluster_with_roi.pdb"
+  "cluster_with_roi"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cluster_with_roi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
